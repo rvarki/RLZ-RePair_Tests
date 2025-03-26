bf51e902cf6c5b776b0bd89e7db54e25a8391b61
Rahul Varki <rvarki@login8.ufhpc>
2025-03-26 17:47:43 -0400

    Update

--- plots/plot_mem_w_phrases.py
+++ plots/plot_mem_w_phrases.py
@@ -5,8 +5,10 @@ import os
 import matplotlib.ticker as ticker
 
 # Uncomment if using SARS
-sorted_phrases = [323494, 1229369, 2437668, 4545892, 9033275, 18035783]
+# sorted_phrases = [323494, 1229369, 2437668, 4545892, 9033275, 18035783]
 
+# Uncommennt if using Chr 19
+sorted_phrases = [104251, 313186, 729688, 1559103, 3234363, 6555312] # Remove 1 and 128
 
 def extract_file_number_and_software(filename):
     match = re.search(r'(\w+)\.(\d+)\.([\w-]+)\.([\w-]+)\.(\w+)', filename)
@@ -45,7 +47,8 @@ def plot_file_numbers(directories, extension, output_basename, sample, multiplie
         for num in all_file_numbers:
             if num in files:  # Check if the file number is available for this software
                 df = pd.read_csv(files[num], delimiter='\t')
-                memory.append(df['max_rss'].iloc[-1])
+                if (num != 128 and num != 1): #TODO: Temp remove later
+                    memory.append(df['max_rss'].iloc[-1])
         
         # Plot the data for this software
         if sorted_phrases:  # Only plot if there are valid numbers
@@ -72,9 +75,9 @@ def plot_file_numbers(directories, extension, output_basename, sample, multiplie
         plt.savefig(f"{output_basename}.{ext}")
 
 # Example usage
-plot_file_numbers(["/blue/boucher/rvarki/rlz-repair_analysis/benchmarks/rlz-repair/SARS"], 
+plot_file_numbers(["/blue/boucher/rvarki/rlz-repair_analysis/benchmarks/rlz-repair/chr19"], 
                     ".compress.benchmark.txt",
-                    "sars_memory_rlz_phrases",
-                    "SARS-CoV-2",
+                    "chr19_memory_rlz_phrases",
+                    "Chromosome 19",
                     False)
 
--- plots/plot_time_w_phrases.py
+++ plots/plot_time_w_phrases.py
@@ -5,8 +5,12 @@ import os
 import matplotlib.ticker as ticker
 
 # Uncomment if using SARS
-sorted_phrases = [323494, 1229369, 2437668, 4545892, 9033275, 18035783]
-update_tree_time = [3, 87, 296, 1012, 4027, 16921]
+#sorted_phrases = [323494, 1229369, 2437668, 4545892, 9033275, 18035783]
+#update_tree_time = [3, 87, 296, 1012, 4027, 16921]
+
+# Uncommennt if using Chr 19
+sorted_phrases = [104251, 313186, 729688, 1559103, 3234363, 6555312] # Remove 1 and 128
+update_tree_time = [2, 6, 11, 28, 53, 131] # Remove 1 and 128
 
 def extract_file_number_and_software(filename):
     match = re.search(r'(\w+)\.(\d+)\.([\w-]+)\.([\w-]+)\.(\w+)', filename)
@@ -45,7 +49,8 @@ def plot_file_numbers(directories, extension, output_basename, sample, multiplie
         for num in all_file_numbers:
             if num in files:  # Check if the file number is available for this software
                 df = pd.read_csv(files[num], delimiter='\t')
-                times.append(df['s'].iloc[-1])
+                if (num != 128 and num != 1): #TODO: Temp remove later
+                    times.append(df['s'].iloc[-1])
         
         # Plot the data for this software
         if sorted_phrases:  # Only plot if there are valid numbers
@@ -75,9 +80,9 @@ def plot_file_numbers(directories, extension, output_basename, sample, multiplie
         plt.savefig(f"{output_basename}.{ext}")
 
 # Example usage
-plot_file_numbers(["/blue/boucher/rvarki/rlz-repair_analysis/benchmarks/rlz-repair/SARS"], 
+plot_file_numbers(["/blue/boucher/rvarki/rlz-repair_analysis/benchmarks/rlz-repair/chr19"], 
                     ".compress.benchmark.txt",
-                    "sars_time_rlz_phrases",
-                    "SARS-CoV-2",
+                    "chr19_time_rlz_phrases",
+                    "Chromosome 19",
                     False)
 
