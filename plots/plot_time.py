2f8a046bd9c0f62f1147701e532967fab0a85b71
Rahul Varki <rvarki@login9.ufhpc>
2025-04-14 19:43:28 -0400

    Update

--- plots/plot_time.py
+++ plots/plot_time.py
@@ -44,7 +44,7 @@ def plot_file_numbers(directories, extension, output_basename, sample, multiplie
     # Get all unique file numbers
     all_file_numbers = sorted(set(num for num, _, _, _ in file_data))
 
-    plt.figure(figsize=(12, 6))
+    plt.figure(figsize=(6.4, 4))
 
     for key, files in software_groups.items():
         sorted_numbers = []
@@ -74,17 +74,20 @@ def plot_file_numbers(directories, extension, output_basename, sample, multiplie
     plt.xticks(all_file_numbers, labels=[str(num) for num in all_file_numbers])
     plt.gca().xaxis.set_major_locator(ticker.FixedLocator(all_file_numbers))
     plt.gca().xaxis.set_minor_locator(ticker.NullLocator())
+    plt.tick_params(axis='both', which='major', labelsize=12)  # for major ticks
+    plt.tick_params(axis='both', which='minor', labelsize=10)  # optional, for minor ticks
 
     # Axis labels
-    plt.xlabel('Haplotypes (1000x) (Log Scale)' if multiplier else 'Haplotypes (Log Scale)')
-    plt.ylabel('Time (s)')
+    plt.xlabel('Number of Sequences (1000x) (Log Scale)' if multiplier else 'Number of Sequences (Log Scale)', fontsize=16)
+    plt.ylabel('Time (s)',fontsize = 16)
     plt.title(sample)
     plt.legend()
+    plt.legend(fontsize=12)  # Adjust the font size of the legend
     plt.grid(True, which="both", linestyle="--")
 
     # Save plot
-    for ext in ['jpeg']:
-        plt.savefig(f"{output_basename}.{ext}")
+    for ext in ['jpeg','pdf','svg','eps']:
+        plt.savefig(f"{output_basename}.{ext}", bbox_inches='tight', dpi=300)
 
 
 plot_file_numbers(
@@ -99,14 +102,15 @@ plot_file_numbers(
     ],
     extension=".compress.benchmark.txt",
     output_basename="sars_time",
-    sample="SARS-CoV-2",
+    sample="",
     multiplier=True,
     rlz_rules=["0.5%", "1%", "2%", "default", "large", None, None]
 )
 
 plot_file_numbers(
     directories=[
-        "/blue/boucher/rvarki/rlz-repair_analysis/benchmarks/rlz-repair/chr19/final_chr19_1_ref",
+        "/blue/boucher/rvarki/rlz-repair_analysis/benchmarks/rlz-repair/chr19/final_5_ref",
+        "/blue/boucher/rvarki/rlz-repair_analysis/benchmarks/rlz-repair/chr19/final_10_ref", 
         "/blue/boucher/rvarki/rlz-repair_analysis/benchmarks/rlz-repair/chr19/final_20_ref",   
         "/blue/boucher/rvarki/rlz-repair_analysis/benchmarks/repair/chr19/normal",
         "/blue/boucher/rvarki/rlz-repair_analysis/benchmarks/repair/chr19/large",
@@ -115,7 +119,7 @@ plot_file_numbers(
     ],
     extension=".compress.benchmark.txt",
     output_basename="chr19_time",
-    sample="Chromosome 19",
+    sample="",
     multiplier=False,
-    rlz_rules=["1 ref","2%", "default", "large", None, None]
+    rlz_rules=["0.5%","1%","2%", "default", "large", None, None]
 )
